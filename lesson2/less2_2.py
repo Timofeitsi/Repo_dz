bcef2aa2527ce26e861d8d5bc0ddf685c5fb3797
Timtsi <2479220@mail.ru>
2020-06-01 11:04:28 +0500

    Удаление ненужного

--- lesson2/less2_2.py
+++
@@ -1,7 +0,0 @@
-# Задача 2
-str = input("Введите элементы списка через звездочку - ")
-my_list = str.split('*')
-print(my_list)
-for i in range(1, len(my_list), 2):
-    my_list[i - 1], my_list[i] = my_list[i], my_list[i - 1]
-print(my_list)
--- lesson2/less2_3_2.py
+++
@@ -1,9 +0,0 @@
-my_dict = {"Зима":[12, 1, 2], "Весна":[4, 5, 3], "Лето":[7, 8, 6], "Осень":[10, 11, 9]}
-a = int(input("Введите порядковый номер месяца - "))
-for key in my_dict:
-    #if a in my_dict[key]:
-    if my_dict[key].count(a):
-        print('Это ', key)
-        break
-else:
-    print("Так то в году 12 месяцев, подумайте еще раз")
